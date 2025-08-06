a93c113957ab580cdae14828a6820557ec118b39
Anxin10 <87345292+Anxin10@users.noreply.github.com>
2025-08-07 04:03:41 +0800

    Update input_ws.js

--- input_ws.js
+++ input_ws.js
@@ -26,7 +26,7 @@ function simulateKeyEvent(key, type = "keydown") {
 function setupInputWebSocket() {
   try {
     // 建立 WebSocket 連線。注意：路徑結尾需要加上 /ws
-    socket = new WebSocket("https://icdc.onrender.com");
+    socket = new WebSocket("wss://icdc.onrender.com/ws");
 
     // 設定連線、關閉、錯誤時的處理函式
     socket.onopen = () => console.log("[WS] 成功連線到伺服器");
@@ -75,4 +75,5 @@ function setupInputWebSocket() {
 document.addEventListener("DOMContentLoaded", () => {
   setupInputWebSocket();
 });
+
 
